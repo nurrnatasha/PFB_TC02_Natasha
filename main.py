1f14aafef08c79644bcf32650e373a00d618b246
yankaiii <s10257095@connect.np.edu.sg>
2024-02-02 13:51:02 +0800

    editing of comments n doc strings and test run again

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# Importing the modules required to manage cash on hand, profit and loss information, and overheads.
+# importing cash on hand, profit and loss, and overheads.
 import cash_on_hand
 import overheads
 import profit_loss
 
 def main():
-    # Process and sort the data, writing results to the summary report
+    # sort the datas, writing results into the summary report
     overheads.sort_overheads()
     cash_on_hand.processcohdata()
     profit_loss.processpaldata()
